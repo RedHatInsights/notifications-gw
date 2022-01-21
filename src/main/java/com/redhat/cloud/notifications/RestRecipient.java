7da39dc619a646bf7775157a0c05a3a88633a23c
Josejulio Martínez <josejulio@users.noreply.github.com>
2022-01-21 01:59:23 -0700

    NOTIF-152 Adding support for recipient.users (#159)

--- src/main/java/com/redhat/cloud/notifications/RestRecipient.java
+++ src/main/java/com/redhat/cloud/notifications/RestRecipient.java
@@ -2,13 +2,26 @@ package com.redhat.cloud.notifications;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.util.List;
+
 public class RestRecipient {
+
+    @JsonProperty("users")
+    private List<String> users;
 
     @JsonProperty("ignore_user_preferences")
     private boolean ignoreUserPreferences;
 
     @JsonProperty("only_admins")
     private boolean onlyAdmins;
+
+    public List<String> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<String> users) {
+        this.users = users;
+    }
 
     public boolean isIgnoreUserPreferences() {
         return ignoreUserPreferences;
