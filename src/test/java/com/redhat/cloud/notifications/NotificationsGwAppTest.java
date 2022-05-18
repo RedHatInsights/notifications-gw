1f221e20b615a92300129e5aa6592b4c0f2ac8a0
Bennet Schulz <mail@bennet-schulz.de>
2022-05-18 10:40:00 +0200

    Add editorconfig

--- src/test/java/com/redhat/cloud/notifications/NotificationsGwAppTest.java
+++ src/test/java/com/redhat/cloud/notifications/NotificationsGwAppTest.java
@@ -4,7 +4,8 @@ import java.util.regex.Pattern;
 import org.junit.jupiter.api.Test;
 
 import static com.redhat.cloud.notifications.NotificationsGwApp.FILTER_REGEX;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class NotificationsGwAppTest {
 
