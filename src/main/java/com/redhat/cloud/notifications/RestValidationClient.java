d98f10f118c1c734b49e9ef29a2dec253cfd1a40
Gwenneg Lepage <gwenneg@users.noreply.github.com>
2023-10-25 08:47:39 +0200

    Replace resteasy classic with resteasy reactive (#361)

--- src/main/java/com/redhat/cloud/notifications/RestValidationClient.java
+++ src/main/java/com/redhat/cloud/notifications/RestValidationClient.java
@@ -4,11 +4,11 @@ import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.Response;
 
 import org.eclipse.microprofile.faulttolerance.Retry;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+import org.jboss.resteasy.reactive.RestQuery;
 
 import static jakarta.ws.rs.core.MediaType.TEXT_PLAIN;
 
@@ -21,6 +21,6 @@ public interface RestValidationClient {
     @Path("/baet")
     @Retry(maxRetries = 5)
     @Produces(TEXT_PLAIN)
-    Response validate(@QueryParam("bundle") String bundle, @QueryParam("application") String application, @QueryParam("eventType") String eventType);
+    Response validate(@RestQuery String bundle, @RestQuery String application, @RestQuery String eventType);
 
 }
