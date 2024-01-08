ff964a8b7b8b2bbd9d75bf199584528baacbcaea
Guillaume Duval <gduval@redhat.com>
2024-01-08 10:19:07 +0100

    Bump to Quarkus 3.6.4

---
+++ src/main/java/com/redhat/cloud/notifications/clowder/GwClowderConfigSource.java
@@ -0,0 +1,96 @@
+package com.redhat.cloud.notifications.clowder;
+
+import com.redhat.cloud.common.clowder.configsource.ClowderConfig;
+import com.redhat.cloud.common.clowder.configsource.ClowderConfigSource;
+import io.quarkus.logging.Log;
+import io.smallrye.config.ConfigValue;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import org.eclipse.microprofile.config.spi.ConfigSource;
+import org.jboss.logging.Logger;
+import static com.redhat.cloud.common.clowder.configsource.ClowderConfigSource.CLOWDER_CONFIG_SOURCE;
+
+/**
+ * A Config source that is using the ClowderAppConfig
+ */
+public class GwClowderConfigSource implements ConfigSource {
+
+    public static final String KAFKA_SASL_JAAS_CONFIG_KEY = "kafka.sasl.jaas.config";
+    public static final String KAFKA_SASL_MECHANISM_KEY = "kafka.sasl.mechanism";
+    public static final String KAFKA_SECURITY_PROTOCOL_KEY = "kafka.security.protocol";
+    public static final String KAFKA_SSL_TRUSTSTORE_LOCATION_KEY = "kafka.ssl.truststore.location";
+    public static final String KAFKA_SSL_TRUSTSTORE_TYPE_KEY = "kafka.ssl.truststore.type";
+    private static final String KAFKA_SASL_JAAS_CONFIG = "kafka.sasl.jaas.config";
+    private static final String KAFKA_SASL_MECHANISM = "kafka.sasl.mechanism";
+    private static final String KAFKA_SECURITY_PROTOCOL = "kafka.security.protocol";
+    private static final String KAFKA_SSL_TRUSTSTORE_LOCATION = "kafka.ssl.truststore.location";
+    private static final String KAFKA_SSL_TRUSTSTORE_TYPE = "kafka.ssl.truststore.type";
+
+
+    private static List<String> KAFKA_SASL_KEYS = List.of(
+            KAFKA_SASL_JAAS_CONFIG_KEY,
+            KAFKA_SASL_MECHANISM_KEY,
+            KAFKA_SECURITY_PROTOCOL_KEY,
+            KAFKA_SSL_TRUSTSTORE_LOCATION_KEY,
+            KAFKA_SSL_TRUSTSTORE_TYPE_KEY
+    );
+
+
+    Logger log = Logger.getLogger(getClass().getName());
+    private static final Map<String,String> props = new HashMap<>();
+
+    private final Map<String, ConfigValue> existingValues;
+
+    public GwClowderConfigSource(ClowderConfigSource conf, Map<String, ConfigValue> exProp) {
+        existingValues = exProp;
+
+        props.put(KAFKA_SECURITY_PROTOCOL, conf.getValue(KAFKA_SECURITY_PROTOCOL));
+        props.put(KAFKA_SASL_MECHANISM, conf.getValue(KAFKA_SASL_MECHANISM));
+        props.put(KAFKA_SASL_JAAS_CONFIG, conf.getValue(KAFKA_SASL_JAAS_CONFIG));
+        props.put(KAFKA_SSL_TRUSTSTORE_LOCATION, conf.getValue(KAFKA_SSL_TRUSTSTORE_LOCATION));
+        props.put(KAFKA_SSL_TRUSTSTORE_TYPE, conf.getValue(KAFKA_SSL_TRUSTSTORE_TYPE));
+    }
+
+    @Override
+    public Map<String, String> getProperties() {
+        Map<String,String> props = new HashMap<>();
+        Set<Map.Entry<String, ConfigValue>> entries = existingValues.entrySet();
+        for (Map.Entry<String,ConfigValue> entry : entries) {
+            String newVal = getValue(entry.getKey());
+            if (newVal == null) {
+                newVal = entry.getValue().getValue();
+            }
+            props.put(entry.getKey(),newVal);
+        }
+
+        return props;
+    }
+
+    @Override
+    public Set<String> getPropertyNames() {
+        Set<String> keys = new HashSet<>(KAFKA_SASL_KEYS);
+        keys.addAll(existingValues.keySet());
+        return keys;
+    }
+
+    @Override
+    public int getOrdinal() {
+        // Provide a value higher than 250 to it overrides application.properties
+        return 265;
+    }
+
+    @Override
+    public String getValue(String s) {
+        return props.get(s);
+    }
+
+
+    @Override
+    public String getName() {
+        return CLOWDER_CONFIG_SOURCE;
+    }
+
+}
